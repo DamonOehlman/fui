34ab07bd5b8f5ebcd085d0db34bfb90590fa9458
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-16 22:37:51 +1000

    Tweaked draw example to use browserify

--- index.js
+++ index.js
@@ -14,53 +14,11 @@ var EventChain = require('./eventchain');
   This is an experimental library to experiment whether handling of events
   can neatly be abstracted away from the actual event handling code itself.
 
-  ## Examples
-
-  There are examples available both in this repository,
-  and online at jsfiddle:
-
-  - [Simple Draw Example - Manual Event Handling](http://jsfiddle.net/DamonOehlman/QuydV/)
-  - [Simple Draw Example - Using Template](http://jsfiddle.net/DamonOehlman/v5ydb/)
-
   ### Example Code
 
-  Here is some very early example code (which is actually running):
+  Here is some example code (which is actually running):
 
-  ```js
-  fui()
-    // only continue if the element matches the selector (uses qwery)
-    .filter('canvas')
-    
-    // convert to relative coordinates
-    .relative()
-    
-    // for each of the targets matched, push a context onto the argument list
-    .each(function(target) {
-        this.args.unshift(target.getContext('2d'));
-    })
-    
-    // handle pointer down events
-    .down(function(context, target, x, y) {
-        this.state.down = true;
-        
-        context.beginPath();
-        context.moveTo(x, y);
-    })
-    
-    // handle pointer move events
-    .move(function(context, target, x, y) {
-        if (this.state.down) {
-            context.lineTo(x, y);
-            context.stroke();
-        }
-    })
-    
-    // handle pointer up events
-    .up(function(context, target, x, y) {
-        this.state.down = false;
-        context.closePath();
-    });
-  ```
+  <<< examples/draw.js
 
   ## Reference
 **/
